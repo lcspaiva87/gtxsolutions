d8dde8589e6208eb7ee12c66c285e57f23f6bcb9
lucas paiva <lcspaiva87@gmail.com>
2023-10-05 22:55:13 -0300

    fix: creating filter and title button

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,14 +1,18 @@
-"use client";
-import { Header } from "@/components/dashboard/header";
-import { SidebarItem } from "@/components/sidebarItem";
-import { siteSettings } from "@/settings/site.settings";
-import { Flex, Avatar } from "@radix-ui/themes";
+import { Icons } from "@/components/icons";
 
 export default function Dashboard() {
-
   return (
-   <main>
-    <p>aqui</p>
-   </main>
+    <div>
+      <div className="flex justify-around p-5">
+        <h1 className="text-xl"> Registro Ocorrência</h1>
+        <div className="flex gap-6">
+          <button className="flex text-gray-300 items-center gap-2 border border-gray-300 p-2 rounded-lg hover:text-gray-400/25 hover:border-gray-400"> Filtro</button>
+          <button className="flex text-gray-300 items-center gap-2 border border-gray-300 p-2 rounded-lg hover:text-gray-400/25 hover:border-gray-400">
+            <Icons.Plus className="w-5 text-gray-400/70" />
+            Nova Ocorrência
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -2,11 +2,13 @@ import { Eye } from "./Eye";
 import { Lock } from "./Lock";
 import { Envelope } from "./Envelope";
 import { User } from "./User";
-import {Search} from "./search"
+import {Search} from "./Search"
+import {Plus} from "./Plus"
 export const Icons = {
   Eye,
   Lock,
   Envelope,
   User,
   Search,
+  Plus
 };
