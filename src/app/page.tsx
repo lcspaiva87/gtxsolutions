62bce4c1c7d1ca41d22ab033125563ba46b9c766
lucas paiva <lcspaiva87@gmail.com>
2023-10-11 15:47:24 -0300

    fix:cretae avata page login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 import { SignintoForm } from "@/components/form/SignintoForm";
 import { Avatar} from "@radix-ui/themes";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="bg-light flex h-screen items-center justify-center sm:bg-gray-100">
       <div className="bg-light m-auto w-full max-w-[420px] rounded p-5 sm:p-8 sm:shadow">
         <div className="mb-2 flex justify-center">
-          <Avatar
-            className="w-8"
-            size="8"
+          <Image
+            className="w-40 h-40 rounded-full "
+            width={100}
+            height={100}
             src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-            fallback="A"
-            radius="full"
+            alt="avatar"
+            quality={100}
           />
         </div>
         <SignintoForm />
