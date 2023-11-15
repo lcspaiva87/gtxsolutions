1f1d3517039d9e84bd9ed518167fcadeedf47098
lucas paiva <lcspaiva87@gmail.com>
2023-11-15 01:41:48 -0300

    Add import for app.scss file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { SignintoForm } from "@/components/form/SignintoForm";
 import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-
+import './scss/app.scss';
 export const metadata: Metadata = {
   title: 'GTX | Login',
   description: '...',
