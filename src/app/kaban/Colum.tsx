34634cdd5f63dfa056d9c3e3981508581146d414
lucas paiva <lcspaiva87@gmail.com>
2023-11-16 14:10:01 -0300

    Fix columnId typo and update task priority

--- src/app/kaban/Colum.tsx
+++ src/app/kaban/Colum.tsx
@@ -2,40 +2,28 @@
 import { useEffect, useState } from "react";
 import { DragDropContext } from "react-beautiful-dnd";
 
+import { Itask } from "@/@types/Task";
 import { useColumns } from "@/hooks/useColuns";
 import { useTask } from "@/hooks/useTask";
 import { ColumItem } from "./ColumItem";
 type Column = {
   id: string;
-  title:string
+  title: string;
 };
 type Assignee = {
   image: string;
   title: string;
- };
+};
 
- type Task = {
-  user: string;
-  columnId: string;
-  message: string;
-  avatar: string;
-  priority: string;
-  company: string;
-  id: string;
-  title: string;
-  startDate: string;
-  endDate: string;
-  assignee: Assignee[];
- };
- const reorderColumnList = (
+const reorderColumnList = (
   columns: Column[],
-  tasks: Task[],
+  tasks: Itask[],
   sourceColId: string,
   destinationColId: string,
   startIndex: number,
   endIndex: number
- ) => {
-  let removed:any; // Define removed here
+) => {
+  let removed: any; // Define removed here
 
   const updatedColumns = columns.map((col) => {
     if (col.id === sourceColId) {
@@ -44,33 +32,40 @@ type Assignee = {
 
       return { ...col, tasks: sourceTasks };
     } else if (col.id === destinationColId) {
-      const destinationTasks = tasks.filter((task) => task.columnId === destinationColId);
+      const destinationTasks = tasks.filter(
+        (task) => task.columnId === destinationColId
+      );
       destinationTasks.splice(endIndex, 0, removed);
 
       return { ...col, tasks: destinationTasks };
     }
-
     return col;
   });
 
   return updatedColumns;
- };
+};
 
 const Column = () => {
-  const { columns: columm, removeMutation: deleteColumnMutation } =
+  const { columns: initialColumns, removeMutation: deleteColumnMutation } =
     useColumns();
-  const { tasks: task, saveMutation, removeMutation } = useTask();
-  const [columns, setColumns] = useState(columm);
-  const [tasks, setTasks] = useState(task);
-  console.log("columns",columns)
-  useEffect(() => {
-    setColumns(columm);
-  }, [columm]);
-  useEffect(() => {
-    setTasks(task);
-  }, [task]);
+  const { tasks: initialTasks, saveMutation, removeMutation } = useTask();
+  const [columns, setColumns] = useState(initialColumns);
+  const [tasks, setTasks] = useState(initialTasks);
 
-  const onDragEnd =  (result: any) => {
+  useEffect(() => {
+    if (initialColumns) {
+      setColumns(initialColumns);
+    }
+  }, [initialColumns]);
+
+  useEffect(() => {
+    if (initialTasks) {
+      setTasks(initialTasks);
+    }
+  }, [initialTasks]);
+  const filterTasks = (columnId: string) =>
+    tasks.filter((task) => task.columnId === columnId);
+  const onDragEnd = (result: any) => {
     const { destination, source } = result;
 
     if (!destination) return;
@@ -92,23 +87,21 @@ const Column = () => {
     );
 
     // Update the columnId of the task
-    const task = tasks.find(task => task.id === result.draggableId);
+    const task = tasks.find((task) => task.id === result.draggableId);
     if (task) {
       saveMutation.mutate({ id: task.id, columnId: destination.droppableId });
       task.columnId = destination.droppableId;
     }
 
     setColumns(updatedColumns);
-   };
+  };
   return (
     <div className="  flex w-full items-center overflow-x-auto overflow-y-hidden pr-[7rem]">
       <DragDropContext onDragEnd={onDragEnd}>
         <div className="flex gap-4">
           {columns.map((column) => {
             // Filtra as tarefas correspondentes a esta coluna
-            const tasksInColumn = task.filter(
-              (task) => task.columnId === column.id
-            );
+            const tasksInColumn = filterTasks(column.id);
 
             return (
               <ColumItem
