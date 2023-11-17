c6d703741c0f1b3543a9c2c788e201d1fc3449bc
lucas paiva <lcspaiva87@gmail.com>
2023-11-17 15:38:34 -0300

    Add http-client module and refactor columns and tasks modules

--- src/data/columns.ts
+++ src/data/columns.ts
@@ -1,14 +1,14 @@
 import { Column } from "@/@types/Column";
-import axios from "axios";
+import { del, get, post } from "./client/http-client";
 
 export const fetchColumns= async (): Promise<Column[]> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/columns`);
+  const response = await get(`${process.env.NEXT_PUBLIC_BASE_URL}/columns`);
   const data = await response.json();
   return data;
 };
 
 export const PostColumns= async ({id,title}:Column) => {
-  const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/columns`,{
+  const response = await post(`${process.env.NEXT_PUBLIC_BASE_URL}/columns`,{
   id,
   title
   });
@@ -16,9 +16,8 @@ export const PostColumns= async ({id,title}:Column) => {
 
 };
 
-
 export const DeleteColumns= async (id:number| string) => {
-  const response = await axios.delete(`${process.env.NEXT_PUBLIC_BASE_URL}/columns/${id}`);
+  const response = await del(`${process.env.NEXT_PUBLIC_BASE_URL}/columns/${id}`);
   return response
 
 };
--- src/data/tasks.ts
+++ src/data/tasks.ts
@@ -1,27 +1,27 @@
 import { Itask } from "@/@types/Task";
-import axios from "axios";
+import { del, get, post, put } from "./client/http-client";
 
 export const fetchTask= async (): Promise<Itask[]> => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/task`);
+  const response = await get(`${process.env.NEXT_PUBLIC_BASE_URL}/task`);
   const data = await response.json();
   return data;
 };
 
 export const postTask= async ({camera,company,file,message,phone,responsible,avatar,columnId,id,priority}:Itask) => {
-  const response = await axios.post(`${process.env.NEXT_PUBLIC_BASE_URL}/task`,{
+  const response = await post(`${process.env.NEXT_PUBLIC_BASE_URL}/task`,{
    camera,company,message,priority,id,columnId,phone,responsible,avatar,file
   });
   return response
 
 };
 export const deleteTaskId= async (id:number | string) => {
-  const response = await axios.delete(`${process.env.NEXT_PUBLIC_BASE_URL}/task/${id}`);
+  const response = await del(`${process.env.NEXT_PUBLIC_BASE_URL}/task/${id}`);
   return response
 
 };
 export const pathTask= async ({id, columnId }:{id:string | number ,columnId:string  | number}) => {
 
-  const response = await axios.patch(`${process.env.NEXT_PUBLIC_BASE_URL}/task/${id}`,{
+  const response = await put(`${process.env.NEXT_PUBLIC_BASE_URL}/task/${id}`,{
     columnId:columnId,
   });
   return response
