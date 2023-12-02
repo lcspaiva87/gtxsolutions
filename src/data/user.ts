c1b394ed6fb91556b9037ef0a7d74132be93c481
lucas paiva <lcspaiva87@gmail.com>
2023-12-02 19:53:34 -0300

    Add user login functionality and useUser hook

--- src/data/user.ts
+++ src/data/user.ts
@@ -1,3 +1,21 @@
-export const fetchUser = async () => {
+import * as yup from 'yup';
+import { get, post } from './client/http-client';
+// Definição do esquema com yup
+const FormSchema = yup.object().shape({
+  email: yup.string().email().required('Digite um email válido'),
+  password: yup.string().max(8).required('Digite uma senha válida'),
+});
 
+// Tipo inferido a partir do esquema do yup
+type YupFormType = yup.InferType<typeof FormSchema>;
+export const LoginUser = async ({email,password}:YupFormType) => {
+  const response = await post('/login',{
+    email,password
+  })
+  return response
+}
+
+export const ListnUser = async () => {
+  const response = await get('/user')
+  return response
 }
---
+++ src/hooks/useUser.ts
@@ -0,0 +1,32 @@
+import { LoginUser } from "@/data/user"
+import { enqueueSnackbar } from "notistack"
+import { useMutation, useQuery, useQueryClient } from "react-query"
+
+export const useUser = () =>{
+  const queryClient = useQueryClient()
+  const createMutation = useMutation({
+    mutationFn:LoginUser,
+    mutationKey: ['user'],
+    onError: () => {
+      enqueueSnackbar('Erro ao tentar fazer login', {
+        variant: 'error',
+      })
+
+    },
+    onSuccess: (_, data) => {}
+  })
+  const {
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['user'],
+
+  })
+  return {
+
+    isLoading,
+    isError,
+    createMutation,
+  }
+}
+
