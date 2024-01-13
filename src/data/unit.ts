48cb665134c32e83942e9df85a6272bf78217306
lucas paiva <lcspaiva87@gmail.com>
2024-01-13 15:07:44 -0300

    Refactor camera-related code to use units

--- src/data/unit.ts
+++ src/data/unit.ts
@@ -4,46 +4,42 @@ import { IUser } from "@/@types/Use";
 import { AxiosResponse } from "axios";
 import Cookies from "js-cookie";
 
-interface AddCamera {
-  ip:string,
-  description: string,
-  site: string,
-  id_camera_group: string,
+interface AddUnit {
+  email: string,
+  password: string,
 }
 
-interface UpdateCameras {
-  id: number,
-  ip:string,
-  description?: string,
-  site?: string,
-  id_camera_group?: string,
+interface UpdateUnit {
+  id:string,
+  email: string,
+  password: string,
 }
 
 
 
-export const addCamera= async ({ description,id_camera_group,ip,site }: AddCamera) => {
+export const addCamera= async ({ email,password }: AddUnit) => {
   const token = Cookies.get("auth_token");
   console.log("token", token)
-  const response = await post<IUser>("/cameras", { description,id_camera_group,ip,site}, token);
+  const response = await post<IUser>("/units", { email,password}, token);
 
   return response;
 };
 export const ListnCameras = async () => {
   const token = Cookies.get("auth_token");
-  const response:AxiosResponse = await get("/cameras",token );
+  const response:AxiosResponse = await get("/units",token );
   return response.data;
 };
 
 
 
 
-export const updateCamera = async ({ id,ip,description,id_camera_group,site}: UpdateCameras) => {
-  const response = await post(`/cameras/${id}`, { ip,description,id_camera_group,site});
+export const updateCamera = async ({ email,id,password}: UpdateUnit) => {
+  const response = await post(`/units/${id}`, { email,password});
   return response;
 };
 
 export const deleteCamera = async (id:string) => {
-  const response = await del(`/cameras/${id}`);
+  const response = await del(`/units/${id}`);
   return response;
 };
 
